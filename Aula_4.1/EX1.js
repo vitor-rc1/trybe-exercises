1bef60f49d13fa3e452d9cc9ad94507eb3dc4160
Vitor Rodrigues <vitor_rc@outlook.com>
2020-08-27 15:29:35 -0300

    primeiro exercicios

---
+++ Aula_4.1/EX1.js
@@ -0,0 +1,12 @@
+var a = 1;
+var b = 2;
+
+console.log(`Soma de a por b = ${a+b}`)
+
+console.log(`Subtração de a por b = ${a-b}`)
+
+console.log(`Multiplicação de a por b = ${a*b}`)
+
+console.log(`Divisão de a por b = ${a/b}`)
+
+console.log(`Modulo de a por b = ${a%b}`)
