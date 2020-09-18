08ef1b2f2b7cf9af668b4734a1e4a54087e690ab
Vitor Rodrigues <vitor_rc@outlook.com>
2020-09-18 15:10:35 -0300

    Finalização do exercício

--- Aula_6.1/script.js
+++ Aula_6.1/script.js
@@ -9,10 +9,32 @@ for (let index = 0; index < estados.length; index += 1) {
   sectionEstados.appendChild(option);
 }
 
-function stopDefAction(evt) {
+function reseumeForm(evt) {
   evt.preventDefault();
-  
+  const formData = document.querySelector('form').elements
+  let result = document.querySelector('#resultado')
+  let radio = ''
+  if(formData[7].checked) {
+    radio = formData[7].value;
+  }
+  else {
+    radio = formData[8].value;
+  }
+
+  result.innerHTML = `
+  <br>
+  Nome completo: ${formData[1].value} <br>
+  Email: ${formData[2].value} <br>
+  CPF: ${formData[3].value} <br>
+  Endereço: ${formData[4].value} <br>
+  Cidade: ${formData[5].value} - Estado: ${formData[6].value} <br>
+  Moradia: ${radio} <br>
+  Resumo do currículo: ${formData[10].value} <br>
+  Cargo: ${formData[11].value} <br>
+  Descrição do cargo: ${formData[12].value} <br>
+  Data de ínicio: ${formData[13].value} <br>
+  `
 }
 
 let submitButton = document.querySelector('#submissao')
-submitButton.addEventListener('click', stopDefAction)
+submitButton.addEventListener('click', reseumeForm)
